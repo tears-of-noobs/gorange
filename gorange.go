e641a8d55518193ea080f1d7d564061f4281778e
Platonov Andrey <poluandrey@gmail.com>
2015-09-25 09:50:39 +0600

    Generate range with different steps

--- gorange.go
+++ gorange.go
@@ -2,14 +2,18 @@ package gorange
 
 import "fmt"
 
-func IntRange(first, last int) ([]int, error) {
+func IntRange(first, last, step int) ([]int, error) {
 	var intRange []int
 	if last < first {
 		msg := "last value must be greater than or equal to a first value"
 		return intRange, fmt.Errorf(msg)
 	}
-	for i := first; i <= last; i++ {
-		intRange = append(intRange, i)
+	value := first
+	stepFactor := 0
+	for value <= last {
+		stepFactor++
+		intRange = append(intRange, value)
+		value = first + step*stepFactor
 	}
 	return intRange, nil
 }
